b46012f784871f1ff60ce20a6f64b8edee154f08
shambhavisharma0110 <shambelieber@gmail.com>
2020-07-23 14:28:53 +0530

    Ex-3

---
+++ 23july.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Php Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello world! This is Shmabhavi Sharma!"
+    echo "<p> <font color = Purple> Name: SHAMBHAVI SHARMA </font></p>";
+    echo "<p> <font color = Blue> Roll No. : 19BIT0281 </font> </p>";
+    echo "<p> <font color = darkblue> Branch: Information Technology </font> </p>";
+    echo "<p> <font color = orange> Credits Registered : 20 </font> </p>";
+    echo "<p> <font color = magenta> This class is cool, cuz I'm contributing so much to github and I like seeing those green marks! </font> </p>";
+    ?>
+</body>
+</html>
